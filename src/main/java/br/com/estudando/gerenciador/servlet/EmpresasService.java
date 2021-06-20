add418e9497eb6cfca98a4f9ccb9de3a8e2496f9
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-06-20 02:26:46 -0300

    Criando Client Service

--- src/main/java/br/com/estudando/gerenciador/servlet/EmpresasService.java
+++ src/main/java/br/com/estudando/gerenciador/servlet/EmpresasService.java
@@ -9,33 +9,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.estudando.gerenciador.modelo.Banco;
 import br.com.estudando.gerenciador.modelo.Empresa;
 
 @WebServlet("/empresas")
-public class EmpresasService extends HttpServlet  {
+public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 		List<Empresa> empresas = new Banco().getEmpresas();
+
+		String tipo = request.getHeader("Accept");
+
+		System.out.println(tipo);
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresas", Empresa.class);
-		String xml = xstream.toXML(empresas);
-		
-		response.setContentType("application/json");
-		response.getWriter().print(xml);
-		
-		/*Gson gson = new Gson();
-		String json = gson.toJson(empresas);
-		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
-		*/
-		
+		if (tipo.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresas", Empresa.class);
+			String xml = xstream.toXML(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(xml);
+		} else if (tipo.contains("json")) {
+
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'sem conteúdo'}");
+		}
+
 	}
 
 }
